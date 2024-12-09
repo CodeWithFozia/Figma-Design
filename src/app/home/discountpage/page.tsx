b37fa8c34a659fe3cb1d8daff5b4a75ce392aee0
CodeWithAnum <anumhussain875@gmail.com>
2024-12-09 23:29:02 +0500

    updated

--- src/app/home/discountpage/page.tsx
+++ src/app/home/discountpage/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const DiscountPage = () => {
     return (
         
-    <div className='lg:w-[1920px] h-[754px]  '>
+    <div className='lg:w-[1820px] h-[754px] overflow-x-hidden wrapper'>
         <Image src={"/image/newsletter-instagram (1).svg"} alt='hero-image' width={1324} height={319}/>
 
     </div>
